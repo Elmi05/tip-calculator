f2681b6fa446701c12a66426ef3a814ccaf62187
Elmi05 <karshe0548@gmail.com>
2023-10-29 15:31:38 +0300

    finished assignment

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+const bill = 275;
+
+let tip;
+if (bill >= 50 && bill <= 300) {
+  tip = bill * 0.15;
+} else {
+  tip = bill * 0.20; 
+}
+
+const totalValue = bill + tip;
+
+const resultString = `The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}.`;
+
+console.log(resultString);
